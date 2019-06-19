75273cae8511c847989f44a0e0664aa180766451
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-06-19 12:05:22 -0300

    Fix some bug in classify_image

--- frog_identifier/classify_image.py
+++ frog_identifier/classify_image.py
@@ -3,12 +3,13 @@ from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
 from keras.layers.normalization import BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator
+from math import ceil
 import matplotlib.pylab as plt
+import keras
 
 # input image dimensions
-img_rows, img_cols = 224, 224
-batch_size = 128
-num_classes = 2
+img_rows, img_cols = 32, 32#256, 256
+batch_size = 32#128
 epochs = 10
 
 def get_data_generator(data_path):
@@ -17,19 +18,19 @@ def get_data_generator(data_path):
     generator = datagen.flow_from_directory(
             data_path,
             target_size=(img_rows, img_cols), # The target_size is the size of your input images,every image will be resized to this size
-            batch_size=32,
+            batch_size=batch_size,
             class_mode='categorical')
 
     return generator
 
 # you shall move you dataset to that file's directory 
-train_generator = get_data_generator('./imagenet/train/')
-validation_generator = get_data_generator('./imagenet/validation/')
+train_generator = get_data_generator('./cifar10/train/')#'./imagenet/train/')
+validation_generator = get_data_generator('./cifar10/validation/')#'./imagenet/validation/')
 
 def create_model():
     # https://www.codeproject.com/Articles/4023566/Cat-or-Not-An-Image-Classifier-using-Python-and-Ke
     model = Sequential()
-    model.add(Conv2D(32, kernel_size = (3, 3), activation='relu', input_shape=(IMAGE_SIZE, IMAGE_SIZE, 1)))
+    model.add(Conv2D(32, kernel_size = (3, 3), activation='relu', input_shape=(img_rows, img_cols, 3)))
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(BatchNormalization())
     model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
@@ -53,32 +54,18 @@ def create_model():
     
     return model
 
-model = createModel()
+model = create_model()
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adam(),
               metrics=['accuracy'])
 
-class AccuracyHistory(keras.callbacks.Callback):
-    # https://adventuresinmachinelearning.com/keras-tutorial-cnn-11-lines/
-    def on_train_begin(self, logs={}):
-        self.acc = []
-
-    def on_epoch_end(self, batch, logs={}):
-        self.acc.append(logs.get('acc'))
-
-history = AccuracyHistory()
-model.fit(train_generator,
-          batch_size=batch_size,
+model.fit_generator(train_generator,
+          steps_per_epoch=(ceil(len(train_generator)/batch_size)),
           epochs=epochs,
           verbose=1,
           validation_data=validation_generator,
-          callbacks=[history])
+          validation_steps=(ceil(len(validation_generator)/batch_size)))
 
-score = model.evaluate(x_test, y_test, verbose=0)
-
+score = model.evaluate_generator(validation_generator, steps=(ceil(len(validation_generator)/batch_size)), verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-plt.plot(range(1, 11), history.acc)
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.show()
