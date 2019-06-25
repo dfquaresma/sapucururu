17fff47ef61791f9f87263212055162f21874401
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-06-25 19:17:25 -0300

    Update validate_model.py

--- validate_model.py
+++ validate_model.py
@@ -1,7 +1,7 @@
 from keras.models import model_from_json
 from keras.preprocessing.image import ImageDataGenerator
 from math import ceil
-import keras
+import keras, sys
 
 # input image dimensions
 batch_size = 128
@@ -21,7 +21,7 @@ def get_test_data_generator(test_data_path, img_rows_cols_tuple):
     test_datagen = ImageDataGenerator()
     test_generator = test_datagen.flow_from_directory(
         test_data_path, # './imagenet/test/'
-        target_size=(int(img_rows_cols_tuple[0]), int(img_rows_cols_tuple[1])), # The target_size is the size of your input images,every image will be resized to this size
+        target_size=img_rows_cols_tuple, # The target_size is the size of your input images,every image will be resized to this size
         batch_size=batch_size,
         class_mode='categorical'
     )
@@ -33,18 +33,27 @@ def evaluate_model(model, test_generator):
         loss=keras.losses.categorical_crossentropy,
         optimizer=keras.optimizers.Adam(),
         metrics=['accuracy']
-    ) 
+    )
     score = model.evaluate_generator(test_generator, steps=(ceil(len(test_generator)/batch_size)), verbose=0)
-    # https://adventuresinmachinelearning.com/keras-tutorial-cnn-11-lines/
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
-
+    return score[0], score[1]
+ 
 if __name__ == '__main__':
-    model_architecture_path = input("Enter the model architecture file path: ") # './trained-models/frog_identifier_cifar10_model_architecture.json'
-    model_weights_path = input("Enter the model weights file path: ") # './trained-models/frog_identifier_cifar10_model_weights.h5'
-    test_data_path = input("Enter the TEST dataset path: ") # './imagenet/test/'
-    img_rows_cols_tuple = input("Enter the image's row and col separated by comma (row,col): ").split(",") # 256,256
+    model_architecture_path = sys.argv[1] # input("Enter the model architecture file path: ") # './trained-models/frog_identifier_cifar10_model_architecture.json'
+    model_weights_path = sys.argv[2] # input("Enter the model weights file path: ") # './trained-models/frog_identifier_cifar10_model_weights.h5'
+    test_data_path = sys.argv[3] # input("Enter the TEST dataset path: ") # './imagenet-to-identification/test/'
+    img_rows_cols_tuple = sys.argv[4].split(",") # input("Enter the image's row and col separated by comma (row,col): ").split(",") # '256,256'
+    
+    repetitions = 10
+    loss_sum, accuracy_sum, count = 0, 0, 0
+    for i in range(repetitions):
+        model = get_pretrained_model(model_architecture_path, model_weights_path)
+        test_generator = get_test_data_generator(test_data_path, (int(img_rows_cols_tuple[0]), int(img_rows_cols_tuple[1])))
+        loss, accuracy = evaluate_model(model, test_generator)
+        count, loss_sum, accuracy_sum = count + 1, loss_sum + loss, accuracy_sum + accuracy
+        print('Count: %d, test loss: %f, test accuracy: %f' % (count, (loss_sum / count), (accuracy_sum / count)))
 
-    model = get_pretrained_model(model_architecture_path, model_weights_path)
-    test_generator = get_test_data_generator(test_data_path, img_rows_cols_tuple)
-    evaluate_model(model, test_generator)
+    final_loss = loss_sum / repetitions
+    final_accuracy = accuracy_sum / repetitions
+    # https://adventuresinmachinelearning.com/keras-tutorial-cnn-11-lines/
+    print('Final test loss:', final_loss)
+    print('Final test accuracy:', final_accuracy)
